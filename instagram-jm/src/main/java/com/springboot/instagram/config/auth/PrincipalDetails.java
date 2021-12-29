fadd94dfd1659dd242534b47908763dfe68111a4
JongDo737 <jongmin373@gmail.com>
2021-12-29 23:38:44 +0900

    시큐리티 연습

--- instagram-jm/src/main/java/com/springboot/instagram/config/auth/PrincipalDetails.java
+++ instagram-jm/src/main/java/com/springboot/instagram/config/auth/PrincipalDetails.java
@@ -14,15 +14,17 @@ import lombok.Data;
 public class PrincipalDetails implements UserDetails{
 	
 	private static final long serialVersionUID = 1L;
-	
+
 	private User user;
 	
 	public PrincipalDetails(User user) {
 		this.user = user;
 	}
-
-	@Override
+	
+	@Override  // 권한 가져다 쓸때 (시큐리티가)
 	public Collection<? extends GrantedAuthority> getAuthorities() {
+		//권한 컬렉션ㄴ -> 리스트 셋의 부모인터페이스이다.
+		// 여러개의 데이터를 컬렉션에 담을 수 있음.\
 		Collection<GrantedAuthority> collection = new ArrayList<GrantedAuthority>();
 		collection.add(new GrantedAuthority() {
 			
@@ -49,25 +51,28 @@ public class PrincipalDetails implements UserDetails{
 	@Override
 	public boolean isAccountNonExpired() {
 		// 계정이 만료되었는지 확인
+		// 예) 한달동안 로그인이 안됐을때 등
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// 비밀번호 5번 이상 틀리면 잠김
+		//비밀번호 5번 이상 틀리면 잠김
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// 자격 증명 만료가 되이 않았는지 확인
+		// 자격 증명 만료거 되지 않았는지 확인
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// 휴먼계정
+		//휴먼계정
 		return true;
 	}
+	
+	
 
 }
--- instagram-jm/src/main/java/com/springboot/instagram/config/auth/PrincipalDetailsService.java
+++ instagram-jm/src/main/java/com/springboot/instagram/config/auth/PrincipalDetailsService.java
@@ -12,17 +12,22 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
-public class PrincipalDetailsService implements UserDetailsService{
-	
+public class PrincipalDetailsService implements UserDetailsService {
+
 	private final UserRepository userRepository;
-	
+
+	// 유저네임(id) 로 검색해서 세션에 저장
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User userEntity = userRepository.getUserByUsername(username);
 		if(userEntity == null) {
-			return null;
+			return null;			
 		}else {
+
 			return new PrincipalDetails(userEntity);
+			//이렇게 하면 알아서 암호화된 비밀번호를 알아내고
+			//알아서 세션객체에 넣어줌 (시큐리티가)
 		}
 	}
+
 }
